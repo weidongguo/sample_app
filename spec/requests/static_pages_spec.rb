0826c6fcc7a3aa60dc0d9abac9a7b40bf54b5c4f
Weidong Guo <weidongguo@live.com>
2014-09-14 02:02:09 -0700

    updated Rspec tests(use have_selector, which allow us to test the presense of specific html. improve Rspec tests, so even more succint | check whether links direct me to the right page(use click_link() | include ApplicationHelper so we don't need ot create a duplicate function for Rspec tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,15 @@
 require 'spec_helper'
-#require '../../app/helpers/application_helper'
-#full_title is a method from spec/support/utilities.rb
+# include ApplicationHelper   <- should be used, but follow the tutorai and put it in utilities.rb in folder support
 
 
-base_title = "Ruby on Rails Tutorial Sample App"
-
+=begin
 describe "Static pages" do
   subject{page}
   describe "Home page" do
     before{ visit root_path } # before method can also be invoked by before(:each) 
     
     it{ should(have_content('Sample App')) }
-    it{ should(have_title(base_title)) } 
+    it{ should(have_title(full_title(''))) } 
     it{ should_not(have_title(' | Home')) }
 
   end
@@ -30,8 +28,67 @@ describe "Static pages" do
   
   describe "Contact page" do
     before { visit contact_path }
-    it{ should have_content "Contact" }
+    it{ should have_selector('h1',  text:"Contact") }
     it{ should have_title full_title 'Contact' }
   end
 end
 
+=end
+
+describe "Static pages" do
+  subject{ page }
+  
+  shared_examples_for "all static pages" do
+    it{ should have_selector('h1', text: heading) }
+    it{ should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before{ visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it{ should_not have_title(' | Home') }
+  end
+
+  describe "Help page" do
+    before{ visit help_path }
+    let(:heading){ "Help" }
+    let(:page_title){ "Help" }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "About page" do
+    before{ visit about_path }
+    let(:heading){ "About" }
+    let(:page_title){ "About" }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Contact page" do
+    before{ visit contact_path }
+    let(:heading){ "Contact" }
+    let(:page_title){ "Contact" }
+
+    it_should_behave_like "all static pages"
+  end
+
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Home"
+    click_link "Sign up now!"
+    should have_title(full_title("Sign Up"))
+    click_link "Contact"
+    should have_title(full_title("Contact"))
+  end
+end
+
+  
