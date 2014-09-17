d61a19d7e689bbab8875c4f1e5a236b3431fa398
Weidong Guo <weidongguo@live.com>
2014-09-16 23:36:53 -0700

    adadded a password_digest column to the db users table &&  added the_secure_password() method (this method does a lot) and password length validation. Finally corresponding Rspec tests to test them

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,18 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # note: \-. is equivalent to \-\. representing char '-' and '.' and not the special metachararcter .   same thing as [\w+] means [\w\+] but \w+ means [\w]+
 
-  validates(:email, presence: true, format: { with:VALID_EMAIL_REGEX }, 
-            uniqueness: { case_sensitive: false } )
+  validates(:email, presence:   true, 
+                    format:     { with:VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensitive: false } )
   
+  validates(:password, length: { minimum: 6 } )
+  has_secure_password 
+  # has_secure_password() does presence validation of password 
+  # and it's confirmation validation 
+  # and more
+  # 1) added password and password_confirmation attribute to the User class
+  # 2) require that they match
+  # 3) add an authenticate method to compared a hashed password to the
+  #    password_digest from the db.
     
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  before { @user= User.new(name: "Example User", email: "user@example.com") }
+  before { @user= User.new(name: "Example User", email: "user@example.com",
+            password:"foobar", password_confirmation: "foobar") }
   subject { @user}
   
   it { should respond_to(:name) }
@@ -9,6 +10,10 @@ describe User do
   it { should be_valid }   # whenever there is a boolean method valid?, there is
                            # a corresponding be_valid test method in Rspec.
                            # In general, foo? -> be_foo 
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   describe "when name is not present" do
     before { @user.name=" " }
@@ -54,4 +59,58 @@ describe User do
     end
       it {should_not be_valid}
   end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com", 
+        password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before {
+      @user.password_confirmation = "mismatch"
+    }
+    it { should_not be_valid }  
+  end
+
+  describe "return value of authenticate method" do
+    before {@user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+    # let() create a local variable with the name of it's arg, which a symbol
+    # So, the local var is named found_user.
+    # then the found_user = the return value from the block
+    # This local var can be used in any of the before and it blocks through
+    # the rest of the test
+    # Finally, let() memoize it's value too. that find_by(...) is called only
+    # once, if the same call happens again, no need to implement the call, the
+    # desired value can be retrieved right off from the cache
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+         # if authenticate succeeded(PW match), an object of User is returned
+         # which should be the same instance of the subject, @user.
+         # if authentication failed, authenticate return false, which won't
+         # match with the object ==> @user (an obj) != false (a boolean value)
+    end
+    
+    describe "with invalid password" do
+      let(:user_for_invalid_password){ found_user.authenticate("invalid") }
+      # in this case, the authenticate returns false
+
+      it { should_not eq user_for_invalid_password }
+      # @user is not boolean value false
+
+      specify { expect(user_for_invalid_password).to be_false }
+      # specify method is the same as the it method, be it sound better in
+      # in this context
+    end
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+  
+
 end
