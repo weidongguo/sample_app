cf35bea701050064f09d0b710a8f4f82b15fa306
Weidong Guo <weidongguo@live.com>
2014-09-17 22:21:22 -0700

    added a better regx to validatte email format, preventing consecutive '.' like wei@gmail..com. | added test to test the before_save { email.downcase! }

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase}
-
+ # before_save { self.email = email.downcase}
+  before_save { email.downcase! }
   validates(:name, presence: true, length: { maximum: 50 } )
   
-  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   # note: \-. is equivalent to \-\. representing char '-' and '.' and not the special metachararcter .   same thing as [\w+] means [\w\+] but \w+ means [\w]+
 
   validates(:email, presence:   true, 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,7 @@ describe User do
   describe "when email format is invalid" do
     it "should be valid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-        foo@baz_baz.com foo@bar+baz.com]
+        foo@baz_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -112,5 +112,13 @@ describe User do
     it { should be_invalid }
   end
   
-
+  describe "email addr with mixed cases" do
+    let(:mixed_case_email) { "Foo@ExAmPle.CoM"}
+    
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+     end
+  end
 end
