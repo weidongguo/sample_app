34d53b7d5782645c00cc3f2eda615c923aa5383b
Weidong Guo <weidongguo@live.com>
2014-09-27 02:10:10 -0700

    turn input type to be password *** for the password text_field, and added a create method in the users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+ 
   end
   
   def index
@@ -8,6 +9,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user= User.new(params[:user]) # not the final implementation!
+    if @user.save
+      # handle a succesfful save
+    else
+      render 'new'
+    end
   end
 
 end
