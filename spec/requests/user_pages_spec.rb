2179be52813e435c069c83de5909a0d54e020958
Weidong Guo <weidongguo@live.com>
2014-09-28 03:12:36 -0700

    added test for after submission with and without valid info & use content_tag() to clean the code for flash

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,6 +36,14 @@ describe "User Pages" do
         it "should not create a user" do
           expect {click_button submit}.not_to change(User, :count)
         end
+        
+        describe "after submission" do
+          before {click_button submit }
+          it{ should have_title "Sign Up" }
+          it{ should have_content "error" }
+
+        end
+
       end
     
       describe "with valid information" do
@@ -48,6 +56,15 @@ describe "User Pages" do
         it "should create an user" do
           expect {click_button submit}.to change(User, :count).by(1)
         end
+        
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user){User.find_by(email: "user@example.com")}
+
+          it {should have_title(user.name) }
+          it {should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
+
       end
    end
 end
