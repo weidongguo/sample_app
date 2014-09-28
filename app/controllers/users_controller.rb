b5aa82c854464423ff1ee5c16cc948c147385b8d
Weidong Guo <weidongguo@live.com>
2014-09-28 01:59:35 -0700

    added flash(welcome message) after signing up! -> finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     @user= User.new(user_params) # not the final implementation!
     if @user.save
-      # handle a succesfful save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user 
     else
       render 'new'
     end
