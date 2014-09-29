80850e087d8475a865de3b79fcfc71858faad790
Weidong Guo <weidongguo@live.com>
2014-09-28 21:34:21 -0700

    passed Rspec test for invalid log-in info (with correct error message being displayed -- exactly persists until a 'request' is made

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    
+    if( user && user.authenticate(params[:session][:password]) )
+       #Sign the user in and redirect ot the user's show page   
+    else
+      flash.now[:error] = "Invalid email/password combination" #not quite right!
+      render 'new' # render doesn't count as a request, so the flash message persists one more request than we want
 
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user= User.new(user_params) # not the final implementation!
+    @user= User.new(user_params) 
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user 
