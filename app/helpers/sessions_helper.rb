600c75b3364ff7b538a9e5d5afec56a91a61c62c
Weidong Guo <weidongguo@live.com>
2014-09-29 02:23:14 -0700

    added implementation of sign_in() so now can succefully sign-in && changed header and added drop_down menu once login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,32 @@
 module SessionsHelper
+  
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token 
+    # the special permanent method set the expiration to 20.years.from_now
+    # cookies[:remember_token][value] = remember_token
+    # cookies[:remember_token][expires] = 20.years.from_now
+
+    # i guess permanent returns an object(type hash) with the
+    # default expires = 20.years.from_now 
+
+    user.update_attribute(:remember_token, User.digest(remember_token) )
+    #user.update_attribute() bypasses validation, unlike user[:remember_token] = ...
+    
+    self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
 end
