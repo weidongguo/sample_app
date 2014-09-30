312a36799a0b484445c6a1e009d85068ba14ca3e
Weidong Guo <weidongguo@live.com>
2014-09-29 22:12:21 -0700

    change to use form_tag() for the sign-page form instead of form_for() and decouple the Rspec tests from the implementation and put them into the spec/support/utilities.rb

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -21,17 +21,15 @@ describe "authentication" do
       
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
+        # it { should_not have_selector('div.alert.alert-error') }
+        it { should have_error_message('Invalid') } 
       end 
     end
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       
-      before {
-        fill_in "Email", with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      }
+      before { valid_signin(user) }
+              
 
       it { should have_title(user.name) }
       it { should have_link('Profile', href:user_path(user)) }
